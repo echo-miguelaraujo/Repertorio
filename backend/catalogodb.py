3ed507245428bcabfb2ba820aad932e0685c98e6
Miguel <miguelangeloarauj@gmail.com>
2026-04-03 14:49:26 -0300

    tabelas: filmes, series e livros criadas com suas respectivas colunas

--- backend/catalogodb.py
+++ backend/catalogodb.py
@@ -2,13 +2,23 @@ import sqlite3
 
 conn = sqlite3.connect('repertorio.db')
 cursor = conn.cursor()
-tabelas = ['filmes','series','livros']
 
-for tabela in tabelas:
-    cursor.execute(f"""
-               CREATE TABLE IF NOT EXISTS {tabela} (
-                   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                   nome VARCHAR
-               )
-               """)
-    conn.commit()
+cursor.execute("""CREATE TABLE IF NOT EXISTS filmes (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR,
+    status VARCHAR,
+    streaming VARCHAR)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS series (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR,
+    temp INTEGER,
+    ep INTEGER,
+    streaming VARCHAR)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS livros (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR,
+    capitulo INTEGER)""")
+
+conn.commit()
